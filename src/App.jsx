f8a71cd355795b88a3e85cdbd78e084e9812601f
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-02-27 18:47:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import Rest from "./Component/Rest";
+
+function App() {
+  return (
+    <>
+      <Rest />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Rest.jsx
@@ -0,0 +1,48 @@
+import React, { version } from "react";
+import { useEffect, useState } from "react";
+
+const Rest = () => {
+  const a = [1, 2, 3, 4];
+  const b = [11, 12, 13, 14];
+  const c = [...a, ...b];
+  console.log(c);
+
+  const obj = { name: "ReactJs", version: 19.3 };
+  const updateObj = { ...obj, version: 20 };
+  console.log(updateObj);
+
+  function sum(...num) {
+    return num.reduce((a, b) => a + b, 0);
+  }
+  const res = sum(1, 2, 3, 4);
+  console.log(res);
+
+  // const user = {
+  //   name: "Jhon",
+  //   age: 30,
+  //   marks: 85,
+  // };
+
+  const { name, ...details } = user;
+  console.log(name);
+  console.log(details);
+
+  const [user, setUser] = useState({ name: "NodeJs", version: 20 });
+  console.log(user);
+
+  function userUpdate() {
+    setUser((prevUser) => ({ ...prevUser, version: 24 }));
+  }
+
+  useEffect(() => {
+    userUpdate();
+  }, []);
+
+  return (
+    <div>
+      <h1>Name: {user.name}</h1>
+    </div>
+  );
+};
+
+export default Rest;
